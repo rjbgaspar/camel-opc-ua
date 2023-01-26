a950cb8d60107f5052647f022f7d392925f6d4f4
rjbgaspar <rjbgaspar@gmail.com>
2023-01-26 10:05:24 +0000

    add second commit

--- src/main/java/com/gv/camelopcua/CamelOpcUaApplication.java
+++ src/main/java/com/gv/camelopcua/CamelOpcUaApplication.java
@@ -61,48 +61,44 @@ class OPCUARouteBuilder extends RouteBuilder {
 	 * the routes using the Java fluent builder syntax.
 	 *
 	 */
+//	@Override
+//	public void configure() {
+//		log.debug("Configuring route for endpoint: {}", config.getEndpoint());
+//
+//		from(config.getEndpoint())
+//				.routeId("Test Route")
+//				.process(exchange -> {
+//					String routeId = exchange.getFromRouteId();
+//					DataValue data = exchange.getIn().getBody(DataValue.class);
+//					log.info("Route '{}': Status: {}, Value: {}",
+//							routeId,
+//							data.getStatusCode().toString(), data.getValue().getValue());
+//				});
+//
+//	}
+
+
 	@Override
 	public void configure() {
-		log.debug("Configuring route for endpoint: {}", config.getEndpoint());
+		String ENDPOINT_URI = "milo-client:opc.tcp://VAL031.gv.local:53530/OPCUA/SimulationServer?allowedSecurityPolicies=None&samplingInterval=500";
 
-		from(config.getEndpoint())
-				.routeId("Test Route")
+		from("direct:start")
+				.setHeader(HEADER_NODE_IDS, constant(Arrays.asList("ns=3;i=1002")))
+				.setHeader(HEADER_AWAIT, constant(true)) // await: parameter "defaultAwaitWrites"
+				.enrich(ENDPOINT_URI, new AggregationStrategy() {
+					@Override
+					public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+						return newExchange;
+					}
+				})
 				.process(exchange -> {
 					String routeId = exchange.getFromRouteId();
 					DataValue data = exchange.getIn().getBody(DataValue.class);
 					log.info("Route '{}': Status: {}, Value: {}",
 							routeId,
 							data.getStatusCode().toString(), data.getValue().getValue());
-				});
-
+				})
+				.to("mock:test1");
+//				.log("Processing ${id}");
 	}
-
-
-//	@Override
-//	public void configure() {
-//		String ENDPOINT_URI = "opc.tcp://VAL031.gv.local:53530/OPCUA/SimulationServer?allowedSecurityPolicies=None&samplingInterval=500";
-//
-//
-////		from("milo-client:opc.tcp://VAL031.gv.local:53530/OPCUA/SimulationServer?allowedSecurityPolicies=None")
-////				.routeId("Test Route")
-////				.setHeader(HEADER_NODE_IDS, constant(Arrays.asList("ns=3;i=1002")))
-////				.process(exchange -> {
-////					String routeId = exchange.getFromRouteId();
-////					DataValue data = exchange.getIn().getBody(DataValue.class);
-////					log.info("Route '{}': Status: {}, Value: {}",
-////							routeId,
-////							data.getStatusCode().toString(), data.getValue().getValue());
-////				});
-////
-//
-//		from("direct:start")
-//				.setHeader(HEADER_NODE_IDS, constant(Arrays.asList("ns=3;i=1002")))
-//				.setHeader(HEADER_AWAIT, constant(true)) // await: parameter "defaultAwaitWrites"
-//				.enrich(ENDPOINT_URI, new AggregationStrategy() {
-//					@Override
-//					public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
-//						return newExchange;
-//					}
-//				}).log("Processing ${id}");
-//	}
 }
