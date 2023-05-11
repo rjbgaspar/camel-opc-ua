ce57d6ef62bb935153ce8a4b99d192c99589a35d
rjbgaspar <rjbgaspar@gmail.com>
2023-05-11 11:04:22 +0100

    extract service

---
+++ src/main/java/com/gv/camelopcua/service/ApacheCamelProducerService.java
@@ -0,0 +1,87 @@
+package com.gv.camelopcua.service;
+
+import com.gv.camelopcua.core.types.builtin.MiloClientMessage;
+import com.gv.camelopcua.service.dto.MiloClientProduceRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.spring.SpringCamelContext;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ushort;
+
+
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ApacheCamelProducerService {
+    final ConcurrentHashMap<String, ProducerTemplate> producers;
+
+    final SpringCamelContext camelContext;
+
+
+    /**
+     * Send the body to an endpoint returning any result output body.
+     * @param miloClientProduceRequestDto
+     * @return
+     */
+    public Map<String, Object> send(MiloClientProduceRequestDto miloClientProduceRequestDto) {
+        log.debug("Request to send body to an endpoint : {}", miloClientProduceRequestDto);
+
+        var producerTemplate = get(miloClientProduceRequestDto.getProduceUri());
+        var list = miloClientProduceRequestDto.getPayload().stream()
+                .map(it -> {
+                    final String endpoint = String.format(producerTemplate.getDefaultEndpoint() + "&defaultAwaitWrites=true&node=RAW(ns=%d;i=%d)",
+                            it.getNodeId().getNamespaceIndex(), it.getNodeId().getIdentifier());
+
+                    // StatusCode{name=Bad_TypeMismatch, value=0x80740000, quality=bad}
+                    // ushort(45)
+                    Variant v = new Variant(ushort((Integer)it.getValue()));
+
+                    // don't write status
+//                     DataValue dataValue = new DataValue(v, null, new DateTime());
+                    // don't write StatusCode or timestamp, most servers don't support it
+                    DataValue dataValue = new DataValue(v, null, null);
+
+                    var serverResponse = producerTemplate.requestBody(endpoint, dataValue, DataValue.class);
+                    log.info("Write \"ns={};i={}\" status : {}",
+                            it.getNodeId().getNamespaceIndex(),
+                            it.getNodeId().getIdentifier(),
+                            serverResponse.getStatusCode().toString());
+                    return new MiloClientMessage(it, serverResponse);
+                }).toList();
+
+        var allGood = list.stream().allMatch(it -> {
+            assert it.getDataValue().getStatusCode() != null;
+            return it.getDataValue().getStatusCode().isGood();
+        });
+
+        return Map.of(
+                "allGood", allGood,
+                "payload", list
+        );
+    }
+
+    private ProducerTemplate get(String produceUri) {
+        return Optional.ofNullable(producers.get(produceUri))
+                .orElseGet(() -> {
+                    var producerTemplate = camelContext.createProducerTemplate();
+                    producerTemplate.setDefaultEndpointUri(produceUri);
+
+                    producers.put(produceUri, producerTemplate);
+                    return producerTemplate;
+                });
+    }
+
+    private <T> T convert(final Object value, final Class<T> clazz) {
+        return camelContext.getTypeConverter().convertTo(clazz, new DataValue(new Variant(value)));
+    }
+}
--- src/main/java/com/gv/camelopcua/web/rest/MiloClientProduceResource.java
+++ src/main/java/com/gv/camelopcua/web/rest/MiloClientProduceResource.java
@@ -2,9 +2,11 @@ package com.gv.camelopcua.web.rest;
 
 
 import com.gv.camelopcua.core.types.builtin.MiloClientMessage;
+import com.gv.camelopcua.service.ApacheCamelProducerService;
 import com.gv.camelopcua.service.dto.MiloClientProduceRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.spring.SpringCamelContext;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
@@ -29,68 +31,15 @@ import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.
  */
 @RestController
 @RequestMapping("/api")
-@Log4j2
+@Slf4j
 @RequiredArgsConstructor
 public class MiloClientProduceResource {
-    final ConcurrentHashMap<String, ProducerTemplate> producers;
-
-    final SpringCamelContext camelContext;
+    private final ApacheCamelProducerService apacheCamelProducerService;
 
 
     @PostMapping("/camel-milo")
     public ResponseEntity<Object> write(@RequestBody MiloClientProduceRequestDto miloClientProduceRequestDto) {
         log.debug("REST request send message exchanges : {}", miloClientProduceRequestDto.getProduceUri());
-
-
-        var producerTemplate = get(miloClientProduceRequestDto.getProduceUri());
-        var list = miloClientProduceRequestDto.getPayload().stream()
-                .map(it -> {
-                    final String endpoint = String.format(producerTemplate.getDefaultEndpoint() + "&defaultAwaitWrites=true&node=RAW(ns=%d;i=%d)",
-                            it.getNodeId().getNamespaceIndex(), it.getNodeId().getIdentifier());
-
-                    // StatusCode{name=Bad_TypeMismatch, value=0x80740000, quality=bad}
-                    // ushort(45)
-                    Variant v = new Variant(ushort((Integer)it.getValue()));
-
-                    // don't write status
-//                     DataValue dataValue = new DataValue(v, null, new DateTime());
-                    // don't write StatusCode or timestamp, most servers don't support it
-                    DataValue dataValue = new DataValue(v, null, null);
-
-                    var serverResponse = producerTemplate.requestBody(endpoint, dataValue, DataValue.class);
-                    log.info("Write \"ns={};i={}\" status : {}",
-                            it.getNodeId().getNamespaceIndex(),
-                            it.getNodeId().getIdentifier(),
-                            serverResponse.getStatusCode().toString());
-                    return new MiloClientMessage(it, serverResponse);
-                }).toList();
-
-        var allGood = list.stream().allMatch(it -> {
-            assert it.getDataValue().getStatusCode() != null;
-            return it.getDataValue().getStatusCode().isGood();
-        });
-
-        return ResponseEntity
-                .ok()
-                .body(Map.of(
-                        "allGood", allGood,
-                        "payload", list
-                ));
-
-    }
-
-    private ProducerTemplate get(String produceUri) {
-        return Optional.ofNullable(producers.get(produceUri))
-                .orElseGet(() -> {
-                    var producerTemplate = camelContext.createProducerTemplate();
-                    producerTemplate.setDefaultEndpointUri(produceUri);
-
-                    producers.put(produceUri, producerTemplate);
-                    return producerTemplate;
-                });
-    }
-
-    private <T> T convert(final Object value, final Class<T> clazz) {
-        return camelContext.getTypeConverter().convertTo(clazz, new DataValue(new Variant(value)));
+        return ResponseEntity.ok().body(apacheCamelProducerService.send(miloClientProduceRequestDto));
     }
 }
