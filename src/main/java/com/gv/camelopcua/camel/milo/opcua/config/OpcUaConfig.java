2e13d53e65ef6a01d7f952b1e2c7857e390d6e92
rjbgaspar <rjbgaspar@gmail.com>
2023-05-13 15:27:22 +0100

    refactor rename for compliance

--- src/main/java/com/gv/camelopcua/camel/milo/opcua/config/OpcUaConfig.java
+++ src/main/java/com/gv/camelopcua/camel/milo/opcua/config/OpcUaConfig.java
@@ -1,6 +1,7 @@
 package com.gv.camelopcua.camel.milo.opcua.config;
 
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +9,10 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class OpcUaConfig {
-
+    @Bean
+    public SpringCamelContext camelContext() {
+        return new SpringCamelContext();
+    }
     @Bean
     public ConcurrentHashMap<String, ProducerTemplate> producers() {
         return new ConcurrentHashMap<>();
